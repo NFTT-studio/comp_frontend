334ad465395f09a3dcb2003d18e41347c8e8f30b
blackjooohn <blackjooohn@gmail.com>
2021-07-06 14:42:21 +0800

    v0.2

---
+++ src/MyToken.js
@@ -0,0 +1,42 @@
+import React from "react";
+import DataApi from "./DataApi";
+import {GridList, GridListTile} from "@material-ui/core";
+
+class RecentlyToken extends React.Component {
+
+
+    constructor() {
+        super();
+        this.state = {
+            tokens:[]
+        };
+    }
+    componentDidMount = async () =>{
+
+        let tokens = await DataApi.pageTokens(0);
+        if(tokens&& tokens.code==0){
+            this.setState({tokens: tokens.data.slice(0,12)});
+        }
+
+    }
+    render() {
+        return (
+
+            <GridList cellHeight={150} cols={9}>
+                {
+
+                    this.state.tokens.map((token,index)=>(
+                        <GridListTile cols={index<3?3:1} key={token.gene} rows={index<3?3:1}>
+                            <img src={token.image} />
+                        </GridListTile>
+                    ))
+                }
+            </GridList>
+
+
+        );
+    }
+
+}
+
+export default RecentlyToken;
---
+++ src/RecentlyToken.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+class MintMask extends React.Component {
+
+
+    constructor() {
+        super();
+        this.state = {
+            tokens:[]
+        };
+    }
+    componentDidMount() {
+
+
+
+    }
+    render() {
+        return (
+            <div>hi</div>
+
+        );
+    }
+
+}
+
+export default MintMask;
