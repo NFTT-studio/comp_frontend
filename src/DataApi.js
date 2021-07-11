ee50763d323601c5444907e772948e2a743903d8
blackjooohn <blackjooohn@gmail.com>
2021-07-11 18:58:04 +0800

    api

--- src/DataApi.js
+++ src/DataApi.js
@@ -1,15 +1,15 @@
 
 class DataApi{
     static pageTokens=async (page)=>{
-        // let uri = "https://api.chineseoperamaskplus.com/assets/list/"+
-            let uri = "http://localhost:8080/assets/list/"+
+        let uri = "https://api.chineseoperamaskplus.com/assets/list/"+
+            // let uri = "http://localhost:8080/assets/list/"+
             (page+1)+"/12";
         let res = await fetch(uri);
         return await res.json();
     }
     static fetchAddressTokens = async (address)=>{
-        // let uri = "https://api.chineseoperamaskplus.com/assets/address/"+address
-        let uri = "http://localhost:8080/assets/address/"+address
+        let uri = "https://api.chineseoperamaskplus.com/assets/address/"+address
+        // let uri = "http://localhost:8080/assets/address/"+address
         let res = await fetch(uri);
         return await res.json();
     }
